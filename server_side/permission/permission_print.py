fb28d07b98c922eb40211d9df0cbebd6bb511374
khoi <khoinp1012@gmail.com>
2026-03-02 15:59:01 +0700

    Security: Remove remaining hardcoded credentials from permission scripts

--- server_side/permission/permission_print.py
+++ server_side/permission/permission_print.py
@@ -2,9 +2,9 @@ from appwrite.client import Client
 from appwrite.services.databases import Databases
 
 APPWRITE_ENDPOINT = "https://syd.cloud.appwrite.io/v1"
-APPWRITE_PROJECT_ID = "687b358f00367ce271e0"
-APPWRITE_API_KEY = "standard_fc341e4e67a647ec4a1901c1b9e74fa0f8101aed67295da7dae235c6a15775ed114d842236818f3e74e87fe5c1a34f11d35bdafc99e74e1759f7ce62a05ee63e852d15204e19f14b7abcec36c214b00a2fb0a62a022ca67b49d26e1cb56f7dcb58269559b54b92dc1cef638d5fed1677f95f67d346eaa3b58ce98f165a4bd536"
-DB_ID = "687ca1a800338d2b13ae"
+APPWRITE_PROJECT_ID = "YOUR_PROJECT_ID_HERE"
+APPWRITE_API_KEY = "YOUR_API_KEY_HERE"
+DB_ID = "YOUR_DATABASE_ID_HERE"
 
 COLLECTIONS = {
     "tire_models": "687ca1a900218b17ed05",
